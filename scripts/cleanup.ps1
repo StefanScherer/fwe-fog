bd57b1b9e6bad06fcf5d0036361c4757102eee99
Stefan Scherer <scherer_stefan@icloud.com>
2020-10-14 08:56:04 +0200

    Webcache löschen

--- scripts/cleanup.ps1
+++ scripts/cleanup.ps1
@@ -1,3 +1,7 @@
+net stop COMSysApp
+taskkill /F /IM dllhost.exe
+taskkill /F /IM taskhost.exe
+taskkill /F /IM taskhostex.exe
 Remove-Item -Force C:\Users\Administrator\AppData\Local\Microsoft\Windows\Webcache\*
 Remove-Item -Force C:\Users\Administrator\AppData\Local\Microsoft\Windows\INetCache\*
 Remove-Item -Force C:\Users\Administrator\AppData\Local\Microsoft\Windows\WebcacheLock.dat
