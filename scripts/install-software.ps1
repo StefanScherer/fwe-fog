5c68752d8e3ba7c8d58db3060660595ad6fdad8b
Stefan Scherer <scherer_stefan@icloud.com>
2020-10-18 23:55:14 +0200

    Automatischer Restart

--- scripts/install-software.ps1
+++ scripts/install-software.ps1
@@ -3,8 +3,9 @@ $ErrorActionPreference = 'Stop'
 # Pakete von https://chocolatey.org installieren
 
 choco install -y libreoffice-fresh
+# choco install -y googlechrome
 choco install -y firefox
 choco install -y 7zip
 choco install -y geogebra-classic.install
 choco install -y zoom
-choco install -y adobereader
+# choco install -y adobereader
--- scripts/sysprep-generalize.ps1
+++ scripts/sysprep-generalize.ps1
@@ -1,20 +1,29 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
-Write-Output "Der sysprep läuft in 15 Sekunden. Jetzt ist noch Zeit zum Abbrechen !!!"
+Write-Output "Der sysprep startet in 15 Sekunden. Jetzt ist noch Zeit zum Abbrechen !!!"
 Start-Sleep 15
 
 Stop-Process -Name sysprep
 Start-Sleep 1
 
+# from https://docs.microsoft.com/de-de/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-copyprofile
+
+# Remove-Item -Recurse HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\*
+# Remove-Item -Recurse HKCU:\Software\Microsoft\Windows\Shell\Associations\FileAssociationsUpdateVersion\*
+# Remove-Item -Recurse HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\*
+
 Start-Process -FilePath C:\Windows\system32\sysprep\sysprep.exe -ArgumentList "/generalize /quit /oobe" -Wait
 
 # from https://docs.microsoft.com/de-de/windows-hardware/manufacture/desktop/customize-the-default-user-profile-by-using-copyprofile
 
-New-PSDrive HKU Registry HKEY_USERS
-Remove-Item -Recurse HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\*
-Remove-ItemProperty HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\FileAssociations\ProgIds -Name "_.html"
-Remove-ItemProperty HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\FileAssociations\ProgIds -Name "_.jpg"
+# Remove-Item -Recurse HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\*
+# Remove-ItemProperty HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\FileAssociations\ProgIds -Name "_.html"
+# Remove-ItemProperty HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\FileAssociations\ProgIds -Name "_.jpg"
 # Remove-Item -Recurse HKU:\.DEFAULT\Software\Microsoft\Windows\Shell\Associations\FileAssociationsUpdateVersion\*
 # Remove-Item -Recurse HKU:\.DEFAULT\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\*
 
-Stop-Computer -Force -Confirm
+# from https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-8.1-and-8/hh825038(v=win.10)
+
+Write-Output "Der Rechner wird in 10 Sekunden heruntergefahren. Jetzt ist noch Zeit zum Abbrechen !!!"
+Start-Sleep 10
+shutdown /s /t 5
