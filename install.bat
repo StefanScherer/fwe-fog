b89726fc84b7d8d2f0565b1b420c7aae54563578
Stefan Scherer <scherer_stefan@icloud.com>
2021-05-30 10:18:17 +0200

    Local run

--- install.bat
+++ install.bat
@@ -1,5 +1,10 @@
 set fogServerIP=192.168.4.2
 
 powershell -Command Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+if exist .\install.ps1 goto local
 curl.exe -o %TEMP%\install.ps1 http://%fogServerIP%/fwe-fog/install.ps1
 powershell -File %TEMP%\install.ps1
+goto done
+:local
+powershell -File .\install.ps1
+:done
